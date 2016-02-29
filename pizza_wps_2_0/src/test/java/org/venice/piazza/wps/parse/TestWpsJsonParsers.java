d8bf5bf3f597f1015b90694c8225a8a5753e5051
bkrasner <bkrasner@radiantblue.com>
2016-02-29 13:46:24 -0500

    add unit test, licensing text

--- pizza_wps_2_0/src/test/java/org/venice/piazza/wps/parse/TestWpsJsonParsers.java
+++ pizza_wps_2_0/src/test/java/org/venice/piazza/wps/parse/TestWpsJsonParsers.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.Before;
@@ -33,17 +34,17 @@ import net.opengis.wps._2.ProcessDescriptionType;
 import net.opengis.wps._2.ProcessOffering;
 import net.opengis.wps._2.ProcessOfferings;;
 
-public class TestWpsXmlParsers {
-	WpsXMLParser parser = null;
+public class TestWpsJsonParsers {
+	WpsJsonParser parser = null;
 	@Before 
 	public void setup() {
-		parser = new WpsXMLParser();
+		parser = new WpsJsonParser();
 		parser.init();
 	}
 
-	@Test
+	//@Test
 	public void testProcessOfferingsParse() {
-		InputStream is = ClassLoader.getSystemResourceAsStream("wpsProcessOfferingsExample.xml");
+		InputStream is = ClassLoader.getSystemResourceAsStream("wpsProcessOfferingsExampleNoNull.json");
 		ProcessOfferings testVal = parser.parseProcessOfferingsInput(is);
 		assertNotNull(testVal);
 		ProcessOffering offering = testVal.getProcessOffering().get(0);
@@ -51,15 +52,18 @@ public class TestWpsXmlParsers {
 		System.out.println(description.getIdentifier().getValue());
 		assertTrue(description.getIdentifier().getValue().contains("Planar-Buffer"));
 		List<InputDescriptionType> inputs = description.getInput();
-		Format format = (Format)(((ComplexDataType)inputs.get(0).getDataDescription().getValue()).getAny().get(0));
-		assertTrue(format.getMimeType().contains("xml"));
-		assertTrue(format.getSchema().contains("feature"));
+		HashMap<String,String> formatMap = (HashMap<String,String>)((ComplexDataType)inputs.get(0).getDataDescription().getValue()).getAny().get(0);
+		//TODO for now JSON doesn't contain enough info to specify Format object, need custom serializer
+		//Format format = (Format)(((ComplexDataType)inputs.get(0).getDataDescription().getValue()).getAny().get(0));
+		assertTrue(formatMap.get("mimeType").contains("xml"));
+		assertTrue(formatMap.get("schema").contains("feature"));
 		
 		MetadataType mdt = (MetadataType)inputs.get(1).getMetadata().get(0).getValue();
 		assertTrue(mdt.getHref().contains("distance"));
-		format = (Format)(((ComplexDataType)description.getOutput().get(0).getDataDescription().getValue()).getAny().get(0));
-		assertTrue(format.getMimeType().contains("xml"));
-		assertTrue(format.getSchema().contains("feature"));
+		//format = (Format)(((ComplexDataType)description.getOutput().get(0).getDataDescription().getValue()).getAny().get(0));
+		formatMap = (HashMap<String,String>)(((ComplexDataType)description.getOutput().get(0).getDataDescription().getValue()).getAny().get(0));
+		assertTrue(formatMap.get("mimeType").contains("xml"));
+		assertTrue(formatMap.get("schema").contains("feature"));
 		mdt = (MetadataType)description.getMetadata().get(3).getValue();
 		assertTrue(mdt.getHref().contains("GML-Buffer"));
 		
@@ -69,7 +73,7 @@ public class TestWpsXmlParsers {
 	
 	@Test
 	public void testExecuteRequestType() {
-		InputStream is = ClassLoader.getSystemResourceAsStream("ExecuteInputs.xml");
+		InputStream is = ClassLoader.getSystemResourceAsStream("ExecuteInputsNoNull.json");
 		ExecuteRequestType testVal = parser.parseExecuteRequestTypeInput(is);
 		assertTrue(testVal.getIdentifier().getValue().contains("Planar-Buffer"));
 		assertTrue(testVal.getInput().get(0).getId().contains("INPUT_GEOMETRY"));
